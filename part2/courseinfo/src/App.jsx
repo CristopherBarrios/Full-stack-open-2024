fc80c6ad6228351df6752beeee1e66b17b0e23a0
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-04 11:00:10 -0600

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,27 +1,4 @@
-
-const Header = ({course}) => <h1>{course.name}</h1>
-const Part = (props) => <p>{props.parte} {props.ejercicio}</p>
-const Content = ({course}) => {
-  return( 
-  <div>
-    {course.parts.map(part => <Part key={part.id} parte={part.name} ejercicio={part.exercises} />)}
-    <Total course={course}/>
-  </div>
-  )
-}
-const Total = ({course}) =>  {
-  const total = course.parts.reduce((s, p) =>  s + p.exercises, 0)
-  return <strong>Total of {total} exercises</strong> 
-}
-
-const Course = ({course}) => {
-  return(
-  <>
-  <Header course={course}/>
-  <Content course={course}/>
-  </>
-  )
-} 
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
