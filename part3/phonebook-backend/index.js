934d89b1b028e203775cb214df25242d849aa078
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-12 10:35:04 -0600

    3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,13 +25,16 @@ let persons = [
 
 ]
 
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
+
   
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
 
   app.get('/api/persons/:id', (request, response) => {
 
@@ -40,10 +43,17 @@ app.get('/', (request, response) => {
 
     if (person) {
       response.json(person)
- } else {
+    } else {
       response.status(404).end()
     }
 
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
   })
   
   
@@ -52,6 +62,7 @@ app.get('/', (request, response) => {
     const numberOfPersons = persons.length;
     response.send(`<p>Phonebook has info for ${numberOfPersons} people</p><p>${date}</p>`);
 });
+
 
   const PORT = 3001
   app.listen(PORT, () => {
