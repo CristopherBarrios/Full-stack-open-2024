4f21f6409cadcb4efe4eea0dc3bbeda3bb15f9e0
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-12 20:01:54 -0600

    3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors')
+
+
 
 let persons = [
   { 
@@ -25,6 +28,7 @@ let persons = [
   }
 ]
 
+app.use(cors())
 app.use(express.json());
 
 morgan.token('postData', (req, res) => {
