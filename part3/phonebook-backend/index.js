04713dc9330f48f8005542104049afaab1ffac30
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-16 14:31:51 -0600

    3.14

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -83,21 +83,15 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
-
-  const person = {
+  const newPerson = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 10000),
-  }
+  });
 
-  persons = persons.concat(person)
 
-  response.json(person)
+  newPerson.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT
