eaeef3663ddcc7a18bd5a5a812474055d407cdf0
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-19 09:28:26 -0600

    3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
 morgan.token('postData', (req, res) => {
   if (req.method === 'POST') {
-    return JSON.stringify(req.body);
+    return JSON.stringify(req.body)
   }
-  return '';
-});
+  return ''
+})
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
@@ -28,16 +28,16 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id;
+  const id = request.params.id
   Person.findById(id)
     .then(person => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -53,70 +53,67 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id;
-  const body = request.body;
+  const id = request.params.id
+  const body = request.body
 
   const person = {
     name: body.name,
-    number: body.number,
-  };
+    number: body.number
+  }
 
   Person.findByIdAndUpdate(id, person, { new: true })
     .then(updatedPerson => {
       if (updatedPerson) {
-        response.json(updatedPerson);
+        response.json(updatedPerson)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch(error => next(error));
-  })
-
-
-app.get('/info', (request, response) => {
-  const date = new Date();
-  Person.countDocuments({}, (err, count) => {
-    response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
-  });
+    .catch(error => next(error))
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date()
+  Person.countDocuments({}, (err, count) => {
+    response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+  })
+})
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number is missing' 
+    return response.status(400).json({
+      error: 'name or number is missing'
     })
   }
 
   const newPerson = new Person({
     name: body.name,
-    number: body.number,
-  });
-
+    number: body.number
+  })
 
   newPerson.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,50 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url = `mongodb+srv://cjrbarrios:${password}@cluster0.ovi7ner.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
-});
+  number: String
+})
 
-const Person = mongoose.model('Person', personSchema);
-
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   // If only password is given, list all entries in the agenda
   Person.find({}).then((result) => {
-    console.log('phonebook:');
+    console.log('phonebook:')
     result.forEach((person) => {
-      console.log(person.name,person.number);
-    });
-    mongoose.connection.close();
-  });
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
   // If password and both name and number are given, add a new entry to the agenda
-  const name = process.argv[3];
-  const number = process.argv[4];
+  const name = process.argv[3]
+  const number = process.argv[4]
 
   const newPerson = new Person({
-    name: name,
-    number: number,
-  });
+    name,
+    number
+  })
 
   newPerson.save().then(result => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-  console.log('Invalid arguments provided.');
-  process.exit(1);
+  console.log('Invalid arguments provided.')
+  process.exit(1)
 }
