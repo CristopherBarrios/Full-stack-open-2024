c8bd2eb0d0447f74cf8d41545af30221abc559e6
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-23 11:19:08 -0600

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,13 +1,31 @@
 const dummy = (blogs) => {
-  return 1;
-  }
+  if (blogs.length === 0) return 1
+}
   
   const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0);
   }
   
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+  
+    let favorite = blogs[0];
+    for (let i = 1; i < blogs.length; i++) {
+      if (blogs[i].likes > favorite.likes) {
+        favorite = blogs[i];
+      }
+    }
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    };
+  }
+
  
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
