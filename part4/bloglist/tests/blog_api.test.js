ff55e87cce91c28c02d76652fed2eb3391460aa7
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-04-24 15:59:37 -0600

    4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('assert')
 const app = require('../app')
 
 const api = supertest(app)
@@ -12,6 +13,16 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('blogs have property id instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    assert.ok(blog.id)
+    assert.ok(!blog._id)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
+
