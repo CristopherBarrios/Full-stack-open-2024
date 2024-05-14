123e2f03731c566d235ff59a2635213db67be433
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-13 19:30:43 -0600

    4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger');
 const config = require('./utils/config')
+const { tokenExtractor } = require('./utils/middleware')
 const app = express()
 
 
@@ -27,6 +28,7 @@ mongoose.connect(config.MONGODB_URI)
   app.use(cors())
   app.use(express.static('dist'))
   app.use(express.json())
+  app.use(tokenExtractor)
 
 
 app.use('/api/blogs', blogsRouter);
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,13 +3,7 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -22,7 +16,8 @@ blogsRouter.get('/', async (request, response) => {
     if (!body.title || !body.url) {
       return response.status(400).json({ error: 'title or url missing' })
     }
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
     }
